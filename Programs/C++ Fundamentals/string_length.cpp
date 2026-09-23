a7cea4b6359aa491f57e986a7d889b93d8049ce1
01fe23bec319 <147500146+01fe23bec319@users.noreply.github.com>
2026-09-23 21:32:06 +0530

    Update string_length.cpp

--- Programs/C++ Fundamentals/string_length.cpp
+++ Programs/C++ Fundamentals/string_length.cpp
@@ -4,10 +4,8 @@
 using namespace std;
 
 int main() {
-    char cString[] = "Hello";
     string cppString = "Hello";
 
-    cout << strlen(cString) << endl;
     cout << cppString.length() << endl;
 
     return 0;
