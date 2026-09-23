a70d808c8af9c933ef4a93c6ef9f40643a3eedd8
01fe23bec319 <147500146+01fe23bec319@users.noreply.github.com>
2026-09-23 21:31:11 +0530

    Update pass_by_pointer.cpp

--- Programs/C++ Fundamentals/pass_by_pointer.cpp
+++ Programs/C++ Fundamentals/pass_by_pointer.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void changeValue(int& x) {
-    x = 100;
+void changeValue(int* x) {
+    *x = 100;
 }
 
 int main() {
     int value = 10;
-    changeValue(value);
+    changeValue(&value);
     cout << value << endl;
     return 0;
 }
